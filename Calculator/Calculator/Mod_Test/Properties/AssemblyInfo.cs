51d2e4d43e64c37568c25804ea08ef79bb632c84
Zlata <zlata.lopata.pm.2022@lpnu.ua>
2024-10-15 00:31:54 +0300

    First commit

---
+++ Calculator/Calculator/Mod_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mod_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mod_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30b4ff8a-d260-4220-9d86-ca775ebe697f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/Calculator/Mod_Test/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using ErrorLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Mod_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Функція має викликати виняток, коли значення a є більше за межі типу int,
+        //а значення результату операції може виходити за ці межі
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMod()
+        {
+            //arrange
+            long a = long.MaxValue; long b = 2;
+            CalcClassBr.CalcClass.Mod(a, b);
+        }
+       
+        [TestMethod]
+        public void TestMod1()
+        {
+            //arrange
+            long a = 3; long b = 2;
+          int actual=  CalcClassBr.CalcClass.Mod(a, b);
+            int expected=1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        //ділення на нуль
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestMod2()
+        {
+            // arrange
+            long a = 3; long b = 0;
+            // act
+            CalcClassBr.CalcClass.Mod(a, b);
+        }
+
+        //Перевірка повідомлення про помилку при ділені на нуль
+        [TestMethod]
+        public void TestMod_DivideByZeroErrorMessage()
+        {
+            // arrange
+            long a = 3; long b = 0;
+            try
+            {
+                // act
+                CalcClassBr.CalcClass.Mod(a, b);
+            }
+            catch (DivideByZeroException x)
+            {
+                // assert
+                Assert.AreEqual(ErrorsExpression.ERROR_09, x.Message);
+            }
+        }
+
+    }
+}
